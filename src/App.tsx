21b647e124a789949920c351649d49c3980b9fdb
naier abassi <naierabassi@gmail.com>
2025-08-20 22:21:39 +0200

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="terminal-container">
+        <text className="terminal-text">
+          terminal:portfolio<span className="terminal-points">:</span>
+          <span className="terminal-tidle">~</span>
+          <span className="terminal-dollars">$</span>
+          <input className="terminal-input"></input>
+        </text>
+      </div>
+    </>
+  );
+}
+
+export default App;
