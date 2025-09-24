96b92a4df5604b2207501f73161e3bc22faec764
nabassi0 <naierabassi@gmail.com>
2025-09-24 10:56:48 +0200

    small changes

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,11 @@
+import "./Loader.css"
+
+export default function Loader() {
+    return (
+        <div className="loader">
+            <div className="spinner">
+                <p className="loading-text">Loading...</p>
+            </div>
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Loader  from './components/Loader/Loader.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Loader />
   </StrictMode>,
 )
