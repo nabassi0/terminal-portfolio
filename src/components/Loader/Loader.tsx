f47e7e38755a2455337c1f1ca2be1f66895e3ce5
nabassi0 <naierabassi@gmail.com>
2025-09-24 22:01:37 +0200

    loading page

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,10 +2,15 @@ import "./Loader.css"
 
 export default function Loader() {
     return (
-        <div className="loader">
-            <div className="spinner">
-                <p className="loading-text">Loading...</p>
-            </div>
+        <div className="content" role="status" aria-live="polite" aria-busy="true">
+            <span className="text-shadows">
+                Loading
+                <span className="dots" aria-hidden="true">
+                    <span className="dot">.</span>
+                    <span className="dot">.</span>
+                    <span className="dot">.</span>
+                </span>
+            </span>
         </div>
     )
 }
