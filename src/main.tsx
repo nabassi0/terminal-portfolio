b3267f8b185ca2397e5eae361895e0deac718eb1
nabassi0 <naierabassi@gmail.com>
2025-10-04 18:51:03 +0200

    creating and adding new things that may not work

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,25 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { StrictMode, useEffect, useState } from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router"
+import "./index.css"
+import Loader from "./components/Loader/Loader"
+import App from "./App"
 
-// Import the generated route tree
-import { routeTree } from './routeTree.gen'
+function Root() {
+  const [showLoader, setShowLoader] = useState(true)
 
-// Create a new router instance
-const router = createRouter({ routeTree })
+  useEffect(() => {
+    const timer = setTimeout(() => setShowLoader(false), 2500)
+    return () => clearTimeout(timer)
+  }, [])
 
-// Register the router instance for type safety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
+  return showLoader ? <Loader /> : <App />
 }
 
-// Render the app
-const rootElement = document.getElementById('root')!
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>,
-  )
-}
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+    <Root />
+    </BrowserRouter>
+  </StrictMode>
+)
